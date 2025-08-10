b3becb0071f009df7cc9d437cd1ab9b39944f716
JahongirEshmurodov <johonbek0808@gmail.com>
2025-08-10 18:49:00 +0500

    fix

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,33 +1,43 @@
-import axios from 'axios'
-import React, { createContext, useState } from 'react'
+import axios from "axios";
+import React, { createContext, useState } from "react";
 
-export const Context = createContext()
+export const Context = createContext();
 function ContextProvider({ children }) {
-  const [data, setdata] = useState()
-  const [loading, setloading] = useState(true)
-  const [error, seterror] = useState(false)
-  async function getWeather(city = 'toshkent') {
+  const [data, setdata] = useState();
+  const [loading, setloading] = useState(true);
+  const [error, seterror] = useState(false);
+  async function getWeather(city = "toshkent") {
     try {
-      setloading(true)
-      let { data } = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${import.meta.env.VITE_APIKEY}`)
-      if(data.length === 0){
-        seterror(true)
-      }else{
-        seterror(false)
+      setloading(true);
+      let { data } = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${
+          import.meta.env.VITE_APIKEY
+        }`
+      );
+      if (data.length === 0) {
+        seterror(true);
+      } else {
+        seterror(false);
       }
-      let { lat, lon, name } = data[0]
-      let response = await axios.get(`https://api.openweathermap.org/data/2.8/onecall?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_APIKEY}&units=metric`)
-      setdata({ ...response.data, name })
-      setloading(false)
+      let { lat, lon, name } = data[0];
+      let response = await axios.get(
+        `https://api.openweathermap.org/data/2.8/onecall?lat=${lat}&lon=${lon}&appid=${
+          import.meta.env.VITE_APIKEY
+        }&units=metric`
+      );
+      setdata({ ...response.data, name });
+      setloading(false);
     } catch (error) {
       console.log(error.message);
-    }finally{
-      setloading(false)
+    } finally {
+      setloading(false);
     }
   }
-  return <Context.Provider value={{ getWeather, data, loading, error }}>
-    {children}
-  </Context.Provider>
+  return (
+    <Context.Provider value={{ getWeather, data, loading, error }}>
+      {children}
+    </Context.Provider>
+  );
 }
 
-export default ContextProvider
+export default ContextProvider;
